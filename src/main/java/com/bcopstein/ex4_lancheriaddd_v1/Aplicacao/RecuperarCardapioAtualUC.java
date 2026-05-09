b273e2d06d6c5d1202705b4423fa08a9e068fbca
Nathalie Jordão de Avila <102388880+NathJav@users.noreply.github.com>
2026-05-09 14:39:59 -0300

    feat: implementa Load Current Menu UC e refatora endpoints - Task #6

---
+++ src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Aplicacao/RecuperarCardapioAtualUC.java
@@ -0,0 +1,21 @@
+package com.bcopstein.ex4_lancheriaddd_v1.Aplicacao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.bcopstein.ex4_lancheriaddd_v1.Aplicacao.Responses.CardapioResponse;
+
+
+@Component
+public class RecuperarCardapioAtualUC {
+    private RecuperarCardapioUC recuperarCardapioUC;
+
+    @Autowired
+    public RecuperarCardapioAtualUC(RecuperarCardapioUC recuperarCardapioUC) {
+        this.recuperarCardapioUC = recuperarCardapioUC;
+    }
+
+    public CardapioResponse run() {
+        long idCardapioAtual = 1; // Supondo ID do cardápio atual seja 1
+        return recuperarCardapioUC.run(idCardapioAtual);
+    }
+}
