edcd83f6d805af536fbf1919418154b518c8bb0e
Gabriel Camargo <bittencourt.gabriel@edu.pucrs.br>
2026-05-11 19:41:08 -0300

    Revert "Feat/tasks 3 e 6 : Implementa UC3 (Carregar Cardápio Atual) e Serviço de Estoque Fake"

--- src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Adaptadores/Apresentacao/CardapioController.java
+++ src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Adaptadores/Apresentacao/CardapioController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bcopstein.ex4_lancheriaddd_v1.Adaptadores.Apresentacao.Presenters.CabecalhoCardapioPresenter;
 import com.bcopstein.ex4_lancheriaddd_v1.Adaptadores.Apresentacao.Presenters.CardapioPresenter;
 import com.bcopstein.ex4_lancheriaddd_v1.Aplicacao.RecuperaListaCardapiosUC;
-import com.bcopstein.ex4_lancheriaddd_v1.Aplicacao.RecuperarCardapioAtualUC;
 import com.bcopstein.ex4_lancheriaddd_v1.Aplicacao.RecuperarCardapioUC;
 import com.bcopstein.ex4_lancheriaddd_v1.Aplicacao.Responses.CardapioResponse;
 import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.Produto;
@@ -23,47 +22,35 @@ import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.Produto;
 public class CardapioController {
     private RecuperarCardapioUC recuperaCardapioUC;
     private RecuperaListaCardapiosUC recuperaListaCardapioUC;
-    private RecuperarCardapioAtualUC recuperaCardapioAtualUC;
 
     public CardapioController(RecuperarCardapioUC recuperaCardapioUC,
-                              RecuperaListaCardapiosUC recuperaListaCardapioUC, RecuperarCardapioAtualUC recuperaCardapioAtualUC) {
+                              RecuperaListaCardapiosUC recuperaListaCardapioUC) {
         this.recuperaCardapioUC = recuperaCardapioUC;
         this.recuperaListaCardapioUC = recuperaListaCardapioUC;
-        this.recuperaCardapioAtualUC = recuperaCardapioAtualUC;
     }
 
-    @GetMapping("/atual")
-    @CrossOrigin("*")
-    public CardapioPresenter recuperaCardapioAtual(){
-        CardapioResponse cardapioResponse = recuperaCardapioAtualUC.run();
-        return mappingToPresenter(cardapioResponse);
-    }
-
-    
     @GetMapping("/{id}")
     @CrossOrigin("*")
     public CardapioPresenter recuperaCardapio(@PathVariable(value="id")long id){
         CardapioResponse cardapioResponse = recuperaCardapioUC.run(id);
-        return mappingToPresenter(cardapioResponse);
+        Set<Long> conjIdSugestoes = new HashSet<>(cardapioResponse.getSugestoesDoChef().stream()
+            .map(produto->produto.getId())
+            .toList());
+        CardapioPresenter cardapioPresenter = new CardapioPresenter(cardapioResponse.getCardapio().getCabecalhoCardapio().titulo());
+        for(Produto produto:cardapioResponse.getCardapio().getProdutos()){
+            boolean sugestao = conjIdSugestoes.contains(produto.getId());
+            cardapioPresenter.insereItem(produto.getId(), produto.getDescricao(), produto.getPreco(), sugestao);
+        }
+        return cardapioPresenter;
     }
 
     @GetMapping("/lista")
     @CrossOrigin("*")
     public List<CabecalhoCardapioPresenter> recuperaListaCardapios(){
-         return recuperaListaCardapioUC.run().cabecalhos().stream()
+         List<CabecalhoCardapioPresenter> lstCardapios = 
+            recuperaListaCardapioUC.run().cabecalhos().stream()
             .map(cabCar -> new CabecalhoCardapioPresenter(cabCar.id(),cabCar.titulo()))
             .toList();
-    }
-
-    private CardapioPresenter mappingToPresenter(CardapioResponse cardapioResponse) {
-        Set<Long> conjIdSugestoes = new HashSet<>(cardapioResponse.getSugestoesDoChef().stream()
-            .map(Produto::getId)
-            .toList());
-        CardapioPresenter cardapioPresenter = new CardapioPresenter(cardapioResponse.getCardapio().getCabecalhoCardapio().titulo());
-        for(Produto produto : cardapioResponse.getCardapio().getProdutos()){
-            boolean sugestao = conjIdSugestoes.contains(produto.getId());
-            cardapioPresenter.insereItem(produto.getId(), produto.getDescricao(), produto.getPreco(), sugestao);
-        }
-        return cardapioPresenter;
+         return lstCardapios;
     }
 }
